661245958044de98be1eb6a7b0179eeae2300a57
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2017-11-09 17:52:38 +0530

    eslint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
+
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+router.get('/', (request, response) => { response.send('Hey! It works!'); });
 
 module.exports = router;
--- start.js
+++ start.js
@@ -4,15 +4,17 @@ const mongoose = require('mongoose');
 require('dotenv').load();
 
 // Connect to our Database and handle any bad connections
-mongoose.connect(process.env.DATABASE, {useMongoClient : true});
+mongoose.connect(process.env.DATABASE, { useMongoClient: true });
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-mongoose.connection.on(
-    'error', (err) => { console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`); });
+mongoose.connection.on('error', (err) => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
 
 // READY?! Let's go!
 
 // Start our app!
 const app = require('./app');
+
 app.set('port', process.env.PORT);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
