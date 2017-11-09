6392d9ee363524e69cc9db176b031028aeaaa00f
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2017-11-09 21:10:03 +0530

    layout

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 // Do work here
-router.get('/', (request, response) => { response.render('hello', { title: 'Hello' }); });
+router.get('/', (request, response) => { response.render('hello', { title: 'Chottu' }); });
 
 module.exports = router;
