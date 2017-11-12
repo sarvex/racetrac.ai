0d433e8bf95aa951170bfdf5ed1e6476af1601dc
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2017-11-12 19:47:29 +0530

    post store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,7 +1,16 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (request, response) => {
   response.render('index', {title: 'Chottu'});
 }
 
 exports.addStore = (request, response) => {
   response.render('editStore', {title: 'Add Store'});
+}
+
+exports.createStore = async (request, response) => {
+  const store = new Store(request.body);
+  await store.save();
+  response.redirect('/');
 }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 const router = express.Router();
 
 // Do work here
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
